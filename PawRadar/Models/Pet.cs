cb02aa7194229679733103bc2ed949d0d233fb22
jyang001 <joeyang095@gmail.com>
2019-12-17 09:52:04 -0500

    add Pet class

---
+++ PawRadar/Models/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Linq;
+using System.Web;
+
+namespace PawRadar.Models
+{   
+
+    [DataContract]
+    public class Pet
+    {
+
+        public String Name { get; set; }
+        
+        public String Species { get; set; }
+
+        public int Age { get; set; }
+
+        public String Location { get; set; }
+
+        public Pet(string name, string species, int age, string location)
+        {
+            Name = name;
+            Species = species;
+            Age = age;
+            Location = location;
+        }
+
+    }
+}
