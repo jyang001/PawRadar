6064f2a2d137e198bb9b1c57274e35e3decb0d57
jyang001 <joeyang095@gmail.com>
2019-12-30 14:02:21 -0500

    add IPet interface

---
+++ PawRadar/Infrastructure/IPet.cs
@@ -0,0 +1,20 @@
+﻿using PawRadar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PawRadar.Infrastructure
+{
+    public interface IPet
+    {
+        List<Pet> GetAll();
+        Pet GetById(int id);
+        void insert(Pet pet);
+        void Update(Pet pet);
+        void Delete(Pet pet);
+        void Save();
+     }
+}
